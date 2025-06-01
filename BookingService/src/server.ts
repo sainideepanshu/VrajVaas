dbe70cce0cc5fa66c71246de81db92ad75a9e254
deepanshu <deepanshukamlesh@gamil.com>
2025-06-01 19:01:38 +0530

    added producer and queue logic for adding notification in Booking Serviec

--- BookingService/src/server.ts
+++ BookingService/src/server.ts
@@ -5,6 +5,7 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { addEmailToQueue } from './producers/email.producer';
 const app = express();
 
 app.use(express.json());
@@ -29,4 +30,16 @@ app.use(genericErrorHandler);
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+
+    for(let i = 0; i < 10; i++) {
+        addEmailToQueue({
+            to: `sample from booking ${i}`,
+            subject: "Sample Email booking",
+            templateId: "sample-template",
+            params: {
+                name: "John Doe",
+                orderId: "12345",
+            }
+        })
+    }
 });
