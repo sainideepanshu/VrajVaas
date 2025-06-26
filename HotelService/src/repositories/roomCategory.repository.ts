6d5fba645f3817ed564c4aa072c2f741ff2f293e
deepanshu <deepanshukamlesh@gamil.com>
2025-06-26 06:48:17 +0530

    added room and room category repositories,services and dto

---
+++ HotelService/src/repositories/roomCategory.repository.ts
@@ -0,0 +1,27 @@
+import RoomCategory from "../db/models/roomCategory";
+import { NotFoundError } from "../utils/errors/app.error";
+import BaseRepository from "./base.repository";
+
+class RoomCategoryRepository extends BaseRepository<RoomCategory> {
+  constructor() {
+    super(RoomCategory);
+  }
+
+  async findAllByHotelId(hotelId: number) {
+    const roomCategories = await this.model.findAll({
+      where: {
+        hotelId: hotelId,
+        deletedAt: null,
+      },
+    });
+
+    if (!roomCategories || roomCategories.length === 0) {
+      throw new NotFoundError(
+        `No room categories found for hotel with id ${hotelId}`
+      );
+    }
+    return roomCategories;
+  }
+}
+
+export default RoomCategoryRepository;
